3ba315c2927ba381f665936e3ca7d97a79fcc7dd
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-08-31 17:08:29 -0400

    add nutrition.json

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
- // Dependencies
-// ***********************************
+// Dependencies 
+// ************************************
 require('dotenv').config();
 
-//const db = require("./models");
-
-const path = require("path");
 const express = require("express");
+const mysql =require("mysql");
 
+const bodyParser = require("body-parser");
+
+// Starts express app
+// ************************************
 const app = express();
 
-// const logger = require("morgan");
-const bodyParser = require('body-parser');
-const request = require("request");
 
-
-const sequelize = require("sequelize");
-// const db = require("./models");
-
-
-
-// const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
+// Middleware 
+// *************************
 // Define Middleware here
 // ************************************
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
+
+// 
 app.use(express.static("public"));
 
 // app.use(express.json({limit: '50mb'}));
@@ -35,32 +29,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
-
-
-// Routes
-// *************************************
-
-
-
-
-
-// Connect to the MySQL DB
-// ***********************************************
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, '..', 'public'))) 
 
-//cons(app)
-//routes(app)
-
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  console.log(" TURBO!")
-});
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(" TURBO!")
+  });
