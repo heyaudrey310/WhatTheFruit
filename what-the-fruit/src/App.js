057702408008a9b8010d8c1001c3d3e306852e6a
Nancy <npmichelle3@gmail.com>
2019-10-03 16:12:58 -0400

    <changes>

--- what-the-fruit/src/App.js
+++ what-the-fruit/src/App.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import Home from "./components/Home";
-import Camera from "./components/Camera";
+import Camera, { FACING_MODES, IMAGE_TYPES } from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css'
 
 
 export default class App extends React.Component {
 
+  onTakePhoto (dataUri) {
+    console.log('takePhoto');
+  }
+ 
+  onCameraError (error) {
+    console.error('onCameraError', error);
+  }
+ 
+  onCameraStart (stream) {
+    console.log('onCameraStart');
+  }
+ 
+  onCameraStop () {
+    console.log('onCameraStop');
+  }
+ 
   render () {
     return (
       <div className="App">
-         <Camera
+        <Camera
           onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+          onCameraError = { (error) => { this.onCameraError(error); } }
+          idealFacingMode = {FACING_MODES.ENVIRONMENT}
+          idealResolution = {{width: 440, height: 280}}
+          imageType = {IMAGE_TYPES.JPG}
+          imageCompression = {0.97}
+          isMaxResolution = {false}
+          isImageMirror = {true}
+          isSilentMode = {false}
+          isDisplayStartCameraError = {true}
+          isFullscreen = {false}
+          sizeFactor = {1}
+          onCameraStart = { (stream) => { this.onCameraStart(stream); } }
+          onCameraStop = { () => { this.onCameraStop(); } }
         />
       </div>
-      // <Home /> 
-    )
-
-   
+    );
   }
 }
